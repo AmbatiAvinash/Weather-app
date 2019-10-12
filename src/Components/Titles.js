6fd021c6e92f23ef8eb733dd46026c10321567de
AmbatiAvinash <avi.ambati07@gmail.com>
2019-10-12 13:28:02 +0530

    Complete weather app

---
+++ src/Components/Titles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Titles extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1 className="title-container__title">Weather Finder App</h1>
+                <p className="title-container__subtitle">Here you can find Weather, conditions and more...</p>
+            </div> 
+        );
+    }
+}
+
+export default Titles;
